6b8d71c1fb9a99866165b1a8d77e18a40b2a79f5
Dennis J de Carvalho <dennisjcarvalho@gmail.com>
2025-02-28 10:08:54 -0300

    alterado nome do diretorio

--- docs/public/js/main.mjs
+++
@@ -1,7 +0,0 @@
-import { allRights } from './modules/allRights.js';
-import { toogleMenu } from './modules/toogleMenu.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  toogleMenu();
-  allRights();
-});
--- docs/public/js/modules/allRights.js
+++
@@ -1,8 +0,0 @@
-const rights = document.querySelector('small#rights');
-const ano = new Date().getFullYear()
-
-export async function allRights() {
-  rights.innerHTML = `
-    &copy; ${ano} - Dennis J de Carvalho
-  `
-}
